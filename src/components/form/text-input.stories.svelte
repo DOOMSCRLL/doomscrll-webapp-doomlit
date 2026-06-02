9672f92f116f8fd76078a3388a4a82d9cced46e5
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-06-03 00:15:46 +0300

    feat: Implement basic text input component with side label

---
+++ src/components/form/text-input.stories.svelte
@@ -0,0 +1,14 @@
+<script lang="ts" module>
+	import { defineMeta } from "@storybook/addon-svelte-csf"
+	import TextInput from "./text-input.svelte"
+
+	const { Story } = defineMeta({
+		title: "Forms/Text Input",
+		component: TextInput,
+	})
+</script>
+
+<Story name="Row" args={{ label: "TEST_LABEL", name: "TEST_INPUT", placeholder: "Test placeholder" }} />
+<Story
+	name="Column"
+	args={{ label: "TEST_LABEL", name: "TEST_INPUT", placeholder: "Test placeholder", layout: "column" }} />
---
+++ src/components/form/text-input.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+	import Icon from "comps/icons/icon.svelte"
+
+	type Props = {
+		name: string
+		label: string
+		placeholder: string
+		value: string
+		inputType?: "text" | "url"
+		layout?: "column" | "row"
+	}
+
+	let { label, name, placeholder, value = $bindable(), inputType = "text", layout = "row" }: Props = $props()
+</script>
+
+<label
+	for={name}
+	class={[
+		"flex h-min w-full font-serif text-2xl font-medium tracking-tighter text-inverse",
+		layout === "row" ? "flex-row items-center gap-4" : "flex-col justify-start",
+	]}>
+	<span class="flex gap-2">
+		<Icon icon="Starmark" size="small" />
+		{label}:
+	</span>
+
+	<input
+		id={name}
+		{name}
+		type={inputType}
+		autocomplete="off"
+		inputmode={inputType}
+		{placeholder}
+		bind:value
+		class={[
+			"w-full border-b-3 border-inverse bg-obverse",
+			"text-center font-serif text-2xl font-medium tracking-tighter text-inverse",
+			"placeholder:text-accent placeholder:italic placeholder:brightness-200",
+		]} />
+</label>
