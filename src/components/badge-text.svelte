9e0b081678cf8fd2ce80adf20619111d97f98141
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-06-14 23:45:43 +0300

    feat: Implement badge text component

---
+++ src/components/badge-text.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	type Props = {
+		text: string
+	}
+
+	const { text }: Props = $props()
+
+	const label = $derived.by(() => {
+		let newLabel = text.trim()
+		if (!newLabel.startsWith("(")) newLabel = "(" + newLabel
+		if (!newLabel.endsWith(")")) newLabel = newLabel + ")"
+		return newLabel
+	})
+</script>
+
+<span class="align-super font-mono text-sm font-bold tracking-widest">{label}</span>
