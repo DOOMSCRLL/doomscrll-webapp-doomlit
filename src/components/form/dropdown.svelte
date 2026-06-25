1f90b948fea901466c9921abeff599c116c6d676
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-06-25 18:55:31 +0300

    refactor: Add flag for hiding native dropdown wrapper's label

--- src/components/form/dropdown.svelte
+++ src/components/form/dropdown.svelte
@@ -12,27 +12,58 @@
 		placeholder: string
 		options: Option[]
 		isRequired?: boolean
+		doHideLabel?: boolean
 		value?: string
 	}
 
-	let { label, name, options, placeholder, isRequired = false, value = $bindable() }: Props = $props()
+	let {
+		label,
+		name,
+		options,
+		placeholder,
+		isRequired = false,
+		doHideLabel = false,
+		value = $bindable(),
+	}: Props = $props()
 </script>
 
-<label
-	for={name}
-	class="flex h-min w-full cursor-text gap-4 font-serif text-2xl font-medium tracking-tighter text-inverse">
-	<span class="flex w-full gap-2">
-		<Icon icon="Starmark" size="small" />
+{#if !doHideLabel}
+	<label
+		for={name}
+		class="flex h-min w-full cursor-text gap-4 font-serif text-2xl font-medium tracking-tighter text-inverse">
+		<span class="flex w-full gap-2">
+			<Icon icon="Starmark" size="small" />
+			{label}:
+		</span>
+
+		<select
+			{name}
+			id={name}
+			required={isRequired}
+			class={[
+				"w-full cursor-pointer border-b-3 border-inverse bg-obverse text-center font-serif",
+				"hover:bg-inverse/40 active:bg-accent",
+			]}
+			bind:value>
+			<option value={undefined} disabled hidden>{placeholder}</option>
+			{#each options as opt (opt)}
+				<option value={opt.value}>{opt.label}</option>
+			{/each}
+		</select>
+	</label>
+{:else}
+	<label for={name} class="sr-only">
 		{label}:
-	</span>
+	</label>
 
 	<select
 		{name}
 		id={name}
 		required={isRequired}
 		class={[
-			"w-full cursor-pointer border-b-3 border-inverse bg-obverse text-center font-serif",
-			"hover:bg-inverse/40 active:bg-accent",
+			"h-12 w-min min-w-0 cursor-pointer rounded-2xl border-3 border-inverse bg-obverse hover:bg-inverse/40 active:bg-accent",
+			"text-center font-mono font-bold text-ellipsis text-inverse uppercase hover:text-obverse active:text-obverse",
+			"overflow-hidden whitespace-nowrap",
 		]}
 		bind:value>
 		<option value={undefined} disabled hidden>{placeholder}</option>
@@ -40,4 +71,4 @@
 			<option value={opt.value}>{opt.label}</option>
 		{/each}
 	</select>
-</label>
+{/if}
