1dc8430ec623beb06c22d0a127fe52f88870e130
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-06-28 20:14:43 +0300

    refactor: Group controls in the profile settings page, and add a button to return to home

--- src/routes/(auth)/profile-settings/+page.svelte
+++ src/routes/(auth)/profile-settings/+page.svelte
@@ -4,6 +4,7 @@
 	import { LocaleContext, ProfileContext } from "contexts/shared.svelte.js"
 	import { getDictionaryOf } from "repos/locale-repo"
 
+	import SlabAnchor from "comps/buttons/slab-anchor.svelte"
 	import SlabButton from "comps/buttons/slab-button.svelte"
 	import TextInput from "comps/form/text-input.svelte"
 	import DoomscrllWordmark from "comps/icons/doomscrll-wordmark.svelte"
@@ -12,30 +13,39 @@
 	let { form } = $props()
 
 	const profile = $derived(ProfileContext.context.value!)
-	const dict = $derived(getDictionaryOf(LocaleContext.context.value!).profileMenu.settings)
+	const allDicts = $derived(getDictionaryOf(LocaleContext.context.value!))
+	const dict = $derived(allDicts.profileMenu.settings)
 </script>
 
 <main class="flex h-full w-full flex-col items-center justify-between px-12 py-4">
 	<DoomscrllWordmark />
 
-	<form method="POST" class="flex w-full flex-col items-center gap-10 px-16" use:enhance>
-		<TextInput
-			name="username"
-			label={dict.username.label}
-			placeholder={dict.username.placeholder}
-			isRequired={profile.username === undefined}
-			value={profile.username} />
-		<TextInput
-			name="description"
-			label={dict.description.label}
-			placeholder={dict.description.placeholder}
-			value={profile.description} />
-		<TextInput name="url" label={dict.url.label} placeholder={dict.url.placeholder} value={profile.url} />
+	<form method="POST" class="flex w-full flex-col items-center gap-12 px-16" use:enhance>
+		<section role="group" class="flex w-full flex-col gap-4">
+			<TextInput
+				name="username"
+				label={dict.username.label}
+				placeholder={dict.username.placeholder}
+				isRequired={profile.username === undefined}
+				value={profile.username} />
+			<TextInput
+				name="description"
+				label={dict.description.label}
+				placeholder={dict.description.placeholder}
+				value={profile.description} />
+			<TextInput name="url" label={dict.url.label} placeholder={dict.url.placeholder} value={profile.url} />
+		</section>
 
-		<SlabButton alignment="left" fit="max" variant="filled" buttonType="submit">
-			<Icon icon="Settings" />
-			{dict.labelCta}
-		</SlabButton>
+		<section role="group" class="flex w-full justify-center gap-4">
+			<SlabAnchor href="/" alignment="left" variant="outlined">
+				<Icon icon="ArrowBack" />
+				{allDicts.common.labelReturnHome}
+			</SlabAnchor>
+			<SlabButton alignment="left" fit="min" variant="filled" buttonType="submit">
+				<Icon icon="Settings" />
+				{dict.labelCta}
+			</SlabButton>
+		</section>
 
 		{#if form?.message}
 			<p class="font-serif text-xl font-medium tracking-wide text-inverse">{form.message}</p>
