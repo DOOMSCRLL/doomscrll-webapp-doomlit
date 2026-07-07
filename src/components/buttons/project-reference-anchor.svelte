7d5ae85a77d61a655cdeaaf76b5f9df6cbb84e2f
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-08 02:17:15 +0300

    feat: Implement DOOMLIT menu as a modal, and related components

---
+++ src/components/buttons/project-reference-anchor.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import type { CreatorProjectEntry } from "models/project"
+
+	import Icon from "comps/icons/icon.svelte"
+	import { DateFmtContext } from "contexts/shared.svelte"
+	import DDate from "utils/d-date"
+	import SlabAnchor from "./slab-anchor.svelte"
+
+	type Props = {
+		entry: CreatorProjectEntry
+	}
+
+	const { entry }: Props = $props()
+
+	let fmt = DateFmtContext.context.value!
+</script>
+
+<li class="flex items-center gap-2 [&>span:first-child_svg]:fill-inverse">
+	<Icon icon="Starmark" />
+	<SlabAnchor href="/doomlits/{entry.referenceId}" variant="outlined" size="normal" alignment="right" fit="min">
+		<span class="font-serif text-2xl font-medium tracking-tight normal-case">{entry.name}</span>
+		•
+		{fmt.getFullDate(DDate.fromISOString(entry.showcaseDate))}
+		<Icon icon="ArrowForward" />
+	</SlabAnchor>
+</li>
---
+++ src/components/doomlits-modal.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { LocaleContext } from "contexts/shared.svelte"
+	import type { CreatorProjectEntry } from "models/project"
+	import { getDictionaryOf } from "repos/locale-repo"
+	import ProjectReferenceAnchor from "./buttons/project-reference-anchor.svelte"
+	import Modal from "./modal.svelte"
+
+	type Props = {
+		projectEntries?: CreatorProjectEntry[]
+		trigger?: HTMLElement
+	}
+
+	let { projectEntries, trigger = $bindable() }: Props = $props()
+	// FIXME: the width of the dialog is off.
+	const dict = $derived(getDictionaryOf(LocaleContext.context.value!).reservation.doomlitMenu)
+</script>
+
+<Modal header={dict.title} closeLabel={dict.labelClose} width="min" bind:trigger>
+	{#if projectEntries}
+		<p class="font-serif text-xl font-medium tracking-tight text-inverse">{dict.copy}</p>
+		<ul class="flex w-fit list-none flex-col gap-4">
+			{#each projectEntries as p (p.referenceId)}
+				<ProjectReferenceAnchor entry={p} />
+			{/each}
+		</ul>
+	{:else}
+		<p class="font-serif text-xl font-medium tracking-tight text-inverse">{dict.noProjectsCopy}</p>
+	{/if}
+</Modal>
