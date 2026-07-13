20fd8e79656bcc480c0f8ccef943ae26707e2ee7
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-14 02:25:29 +0300

    style/refactor: Tweak `FeatureChip` to mimic `TagChip`

--- src/components/form/data-chips/feature-chip.svelte
+++ src/components/form/data-chips/feature-chip.svelte
@@ -1,26 +1,34 @@
 <script lang="ts">
-	import SlabButton from "comps/buttons/slab-button.svelte"
-	import Icon from "comps/icons/icon.svelte"
 	import { LocaleContext } from "contexts/shared.svelte"
 	import { getDictionaryOf } from "repos/locale-repo"
 	import { generateColorFrom, hslStrToCss } from "utils/generate-color-from"
 
+	import SlabButton from "comps/buttons/slab-button.svelte"
+	import Icon from "comps/icons/icon.svelte"
+
 	type Props = {
 		feature: string
-		index: number
-		onRemove: (index: number) => void
+		onRemove: (feature: string) => void
 	}
 
-	const { feature, index, onRemove }: Props = $props()
+	const { feature, onRemove }: Props = $props()
 
 	const removeLabel = $derived(getDictionaryOf(LocaleContext.context.value!).common.dataChips.featureChip.labelRemove)
 </script>
 
-<li class="flex items-center gap-4">
-	<span class="rounded-full px-4 py-2 text-inverse" style="background-color: {hslStrToCss(generateColorFrom(feature))}">
-		{feature}
-	</span>
-	<SlabButton variant="text" size="small" fit="square" ariaLabel={removeLabel} onClick={() => onRemove(index)}>
-		<Icon icon="Remove" size="small" />
+<li class="flex w-min items-center justify-start">
+	<div class="rounded-full px-4 py-2" style="background-color: {hslStrToCss(generateColorFrom(feature))}">
+		<p class="font-serif text-xl font-bold whitespace-nowrap text-obverse">
+			{feature}
+		</p>
+	</div>
+	<SlabButton
+		variant="text"
+		hasAccent={true}
+		fit="square"
+		ariaLabel={removeLabel}
+		size="normal"
+		onClick={() => onRemove(feature)}>
+		<Icon icon="Cancel" size="normal" />
 	</SlabButton>
 </li>
