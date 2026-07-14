a2847aac5bb1dc55c50e88e455d5b0462b95c4fd
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-14 22:51:04 +0300

    refactor: Update `Tooltip` component to add support for new `positionTooltipTo` attachment

--- src/components/tooltip.svelte
+++ src/components/tooltip.svelte
@@ -1,17 +1,26 @@
 <script lang="ts">
-	import { positionTooltipTo } from "attcs/position-tooltip-to"
 	import type { Snippet } from "svelte"
 	import { fade } from "svelte/transition"
+
+	import { positionTooltipTo, type TooltipAlignment } from "attcs/position-tooltip-to"
+	import ttm from "utils/type-literal-to-class"
 
 	type Props = {
 		id?: string
 		isOpen?: boolean
 		trigger?: HTMLElement
 		children?: Snippet
+		alignment?: TooltipAlignment
 	}
-	const { children, trigger, isOpen = false, id }: Props = $props()
+	const { children, trigger, isOpen = false, id, alignment = "right" }: Props = $props()
 
 	const tooltipId = $derived(id ?? `tooltip_${Math.random().toString(36).slice(2, 9)}`)
+	let innerAlignment = $state<TooltipAlignment>()
+	let renderedAlignment = $derived(innerAlignment ?? alignment)
+
+	function handlePositionChange(a: TooltipAlignment) {
+		innerAlignment = a
+	}
 </script>
 
 {#if isOpen && trigger}
@@ -19,11 +28,15 @@
 		role="tooltip"
 		id={tooltipId}
 		class={[
-			"absolute z-50 flex h-min w-fit max-w-40/100 flex-col items-start justify-center gap-2",
-			"arrow-decor-left ml-4 rounded-3xl border-3 border-inverse bg-obverse p-4",
+			"fixed z-50 flex h-min w-fit max-w-40/100 flex-col items-start justify-center gap-2",
+			"rounded-3xl border-3 border-inverse bg-obverse p-4",
 			"pointer-events-none",
+			ttm(renderedAlignment, {
+				left: "arrow-decor-right",
+				right: "arrow-decor-left",
+			}),
 		]}
-		{@attach positionTooltipTo({ anchor: trigger })}
+		{@attach positionTooltipTo({ anchor: trigger, alignment, onPositionChange: handlePositionChange })}
 		transition:fade={{ duration: 150 }}>
 		{@render children?.()}
 	</div>
