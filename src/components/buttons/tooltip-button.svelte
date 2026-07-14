8e7d8096795f0333cb7da7ece8ddeb8cac0c2ece
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-14 22:47:44 +0300

    refactor: Add a alignment prop to `TooltipButton`

--- src/components/buttons/tooltip-button.svelte
+++ src/components/buttons/tooltip-button.svelte
@@ -1,12 +1,19 @@
 <script lang="ts">
 	import { LocaleContext } from "contexts/shared.svelte"
 	import { getDictionaryOf } from "repos/locale-repo"
+	import type { TooltipAlignment } from "attcs/position-tooltip-to"
 
 	import Icon from "comps/icons/icon.svelte"
 	import Tooltip from "comps/tooltip.svelte"
 	import SlabButton from "./slab-button.svelte"
 
-	const { content, id } = $props()
+	type Props = {
+		content: string
+		id?: string
+		alignment?: TooltipAlignment
+	}
+
+	const { content, id, alignment = "right" }: Props = $props()
 
 	const tooltipLabel = $derived(getDictionaryOf(LocaleContext.context.value!).common.labelTooltip)
 
@@ -36,6 +43,6 @@
 	<Icon icon="Help" size="small" />
 </SlabButton>
 
-<Tooltip {id} isOpen={isTooltipActive} trigger={buttonRef}>
+<Tooltip {id} isOpen={isTooltipActive} trigger={buttonRef} {alignment}>
 	<p class="whitespace-pre-line">{content}</p>
 </Tooltip>
