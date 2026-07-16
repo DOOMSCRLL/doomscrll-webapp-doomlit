18da1a9dea6feab37f6d148294cf6f5f382d7ec3
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-16 18:26:57 +0300

    wip: Add management menu and publish button to the DOOMLIT route

---
+++ src/components/form/manage-menu.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import SlabButton from "comps/buttons/slab-button.svelte"
+	import Icon from "comps/icons/icon.svelte"
+	import Popover from "comps/popover.svelte"
+	/*
+  type Props = {
+
+  }
+
+  const {  }: Props = $props()*/
+	// TODO: IMPLEMENT PROJECT MANAGEMENT MENU
+	let buttonRef = $state<HTMLButtonElement>()
+</script>
+
+<SlabButton alignment="right" fit="min" size="normal" variant="text" bind:reference={buttonRef}>
+	MISSING_LABEL
+	<Icon icon="ArrowDropdown" size="small" />
+</SlabButton>
+<Popover
+	arrowPosition="top"
+	horizontalAlignment="center"
+	verticalAlignment="bottom"
+	doManageTrigger={true}
+	bind:trigger={buttonRef}>
+	<p>MISSING_CONTENT</p>
+</Popover>
--- src/routes/doomlits/[referenceId]/+page.svelte
+++ src/routes/doomlits/[referenceId]/+page.svelte
@@ -12,10 +12,12 @@
 	import DDate from "utils/d-date"
 
 	import BrandNav from "comps/brand-nav.svelte"
+	import SlabButton from "comps/buttons/slab-button.svelte"
 	import CopyableText from "comps/copyable-text.svelte"
 	import Dropdown from "comps/form/dropdown.svelte"
 	import FeatureDdropdown from "comps/form/feature-ddropdown.svelte"
 	import ImgInput from "comps/form/img-selector/img-input.svelte"
+	import ManageMenu from "comps/form/manage-menu.svelte"
 	import PlatformDdropdown from "comps/form/platform-ddropdown.svelte"
 	import TagDdropdown from "comps/form/tag-ddropdown.svelte"
 	import TextArea from "comps/form/text-area.svelte"
@@ -64,73 +66,74 @@
 <main class="flex h-screen flex-col overflow-hidden px-2 pb-2 supports-[height:100dvh]:h-dvh">
 	<BrandNav comps={{ date: showcaseDate, returnHref: "/" }} bind:helpModalTrigger />
 	<section class="h-full w-full overflow-hidden rounded-3xl border-4 border-inverse p-4 pr-1">
-		<div
-			class={[
-				"flex h-full w-full flex-col items-start justify-start gap-12 overflow-y-auto",
-				"[scrollbar-color:var(--color-accent)_transparent]",
-			]}>
-			<section class="flex items-center gap-4">
-				<p class="font-serif text-2xl font-medium text-inverse">
-					<Icon icon="Starmark" size="small" />
-					{formDict.refId.label}:
-				</p>
+		<form
+			action="?/publish"
+			class="grid h-full w-full [scrollbar-color:var(--color-accent)_transparent] auto-rows-min grid-cols-2 justify-items-center gap-12 overflow-y-auto">
+			<section class="flex w-full items-center gap-4">
+				<Icon icon="Starmark" size="small" />
+				<p class="font-serif text-2xl font-medium text-inverse">{formDict.refId.label}:</p>
 				<CopyableText content={project.referenceId} />
 			</section>
-			<form action="" class="grid h-full w-full auto-rows-min grid-cols-2 justify-items-center gap-12">
-				<TextInput
-					name="project-name"
-					inputType="text"
-					label={formDict.name.label}
-					placeholder={formDict.name.placeholder}
-					isRequired={true}
-					value={project.name} />
-				<Dropdown
-					name="project-category"
-					label={formDict.category.label}
-					placeholder={formDict.category.placeholder}
-					options={getCategories().map((c) => ({ label: getCategoryLabelFor(c, locale), value: c }))}
-					isRequired={true}
-					bind:value={category} />
-				<TagDdropdown {category} {selectedTags} maxTagCount={data.rules.maxTagCount} />
-				<PlatformDdropdown {category} {primaryPlatform} {selectedPlatforms} />
-				<ImgInput
-					name="project-cover"
-					label={formDict.coverImg.label}
-					placeholder={formDict.coverImg.placeholder}
-					instructions={formDict.coverImg.instructions}
-					tooltip={formDict.coverImg.tooltip}
-					imageType="cover"
-					isRequired={true}
-					maxFileSizeMB={data.rules.maxImageFileSizeMB}
-					initialUrls={project.coverImagePath ? [project.coverImagePath] : []} />
-				<TextArea
-					name="project-description"
-					label={formDict.description.label}
-					placeholder={formDict.description.placeholder}
-					instructions={formDict.description.instructions}
-					value={project.description ?? undefined} />
-				<YoutubeVideoInput
-					name="project-trailer-url"
-					label={formDict.video.label}
-					placeholder={formDict.video.placeholder}
-					instructions={formDict.video.instructions}
-					url={project.videoUrl ?? undefined} />
-				<ImgInput
-					name="project-screenshots"
-					label={formDict.screenshots.label}
-					placeholder={formDict.screenshots.placeholder}
-					instructions={formDict.screenshots.instructions}
-					tooltip={formDict.screenshots.tooltip}
-					imageType="screenshot"
-					canSelectMultiple={true}
-					maxImages={data.rules.maxScreenshotCount}
-					maxFileSizeMB={data.rules.maxImageFileSizeMB}
-					initialUrls={project.screenshotPaths ?? []} />
-				<section class="col-[span_2] w-full">
-					<FeatureDdropdown {category} {selectedFeatures} />
-				</section>
-			</form>
-		</div>
+			<section class="flex w-full items-center justify-end gap-4">
+				<SlabButton variant="filled" fit="min" alignment="left" isDisabled={true}>
+					<Icon icon="Upload" />
+					MISSING_LABEL
+				</SlabButton>
+				<ManageMenu />
+			</section>
+			<TextInput
+				name="project-name"
+				inputType="text"
+				label={formDict.name.label}
+				placeholder={formDict.name.placeholder}
+				isRequired={true}
+				value={project.name} />
+			<Dropdown
+				name="project-category"
+				label={formDict.category.label}
+				placeholder={formDict.category.placeholder}
+				options={getCategories().map((c) => ({ label: getCategoryLabelFor(c, locale), value: c }))}
+				isRequired={true}
+				bind:value={category} />
+			<TagDdropdown {category} {selectedTags} maxTagCount={data.rules.maxTagCount} />
+			<PlatformDdropdown {category} {primaryPlatform} {selectedPlatforms} />
+			<ImgInput
+				name="project-cover"
+				label={formDict.coverImg.label}
+				placeholder={formDict.coverImg.placeholder}
+				instructions={formDict.coverImg.instructions}
+				tooltip={formDict.coverImg.tooltip}
+				imageType="cover"
+				isRequired={true}
+				maxFileSizeMB={data.rules.maxImageFileSizeMB}
+				initialUrls={project.coverImagePath ? [project.coverImagePath] : []} />
+			<TextArea
+				name="project-description"
+				label={formDict.description.label}
+				placeholder={formDict.description.placeholder}
+				instructions={formDict.description.instructions}
+				value={project.description ?? undefined} />
+			<YoutubeVideoInput
+				name="project-trailer-url"
+				label={formDict.video.label}
+				placeholder={formDict.video.placeholder}
+				instructions={formDict.video.instructions}
+				url={project.videoUrl ?? undefined} />
+			<ImgInput
+				name="project-screenshots"
+				label={formDict.screenshots.label}
+				placeholder={formDict.screenshots.placeholder}
+				instructions={formDict.screenshots.instructions}
+				tooltip={formDict.screenshots.tooltip}
+				imageType="screenshot"
+				canSelectMultiple={true}
+				maxImages={data.rules.maxScreenshotCount}
+				maxFileSizeMB={data.rules.maxImageFileSizeMB}
+				initialUrls={project.screenshotPaths ?? []} />
+			<section class="col-[span_2] w-full">
+				<FeatureDdropdown {category} {selectedFeatures} />
+			</section>
+		</form>
 	</section>
 </main>
 
