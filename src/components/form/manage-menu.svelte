39e5441537d23a4d6fb408a7e0603d6bfb5c8d1d
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-16 18:58:31 +0300

    feat/wip: Implement project management menu

--- src/components/form/manage-menu.svelte
+++ src/components/form/manage-menu.svelte
@@ -2,6 +2,8 @@
 	import SlabButton from "comps/buttons/slab-button.svelte"
 	import Icon from "comps/icons/icon.svelte"
 	import Popover from "comps/popover.svelte"
+	import { LocaleContext } from "contexts/shared.svelte"
+	import { getDictionaryOf } from "repos/locale-repo"
 	/*
   type Props = {
 
@@ -9,11 +11,13 @@
 
   const {  }: Props = $props()*/
 	// TODO: IMPLEMENT PROJECT MANAGEMENT MENU
+	const dict = $derived(getDictionaryOf(LocaleContext.context.value!).doomlits.manage)
+
 	let buttonRef = $state<HTMLButtonElement>()
 </script>
 
 <SlabButton alignment="right" fit="min" size="normal" variant="text" bind:reference={buttonRef}>
-	MISSING_LABEL
+	{dict.label}
 	<Icon icon="ArrowDropdown" size="small" />
 </SlabButton>
 <Popover
