474e7d15929b8f84c0db074d5a41ca4b960137d9
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-18 02:10:57 +0300

    refactor: Update BFF endpoint for fetching upload URLs to include a prop for `locale`, conforming to backend changes

--- src/routes/api/doomlits/[referenceId]/upload-urls/+server.ts
+++ src/routes/api/doomlits/[referenceId]/upload-urls/+server.ts
@@ -4,8 +4,8 @@ import { getUploadUrls } from "repos/project-repo"
 
 import type { RequestHandler } from "./$types"
 
-export const GET: RequestHandler = async ({ params, url, fetch }) => {
+export const GET: RequestHandler = async ({ params, url, locals, fetch }) => {
 	const screenshotCount = Number(url.searchParams.get("screenshotCount") ?? "0")
-	const data = await getUploadUrls(params.referenceId, screenshotCount, fetch)
+	const data = await getUploadUrls(params.referenceId, screenshotCount, locals.locale, fetch)
 	return json(data)
 }
