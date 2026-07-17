cee6a440d029841eee6cbdb12ca376c46f754ae0
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-18 02:08:44 +0300

    refactor: Update `APIResponse` model to reflect the API

--- src/models/internal/api.ts
+++ src/models/internal/api.ts
@@ -4,4 +4,4 @@ export type APIErrorDetails = {
 	details: Record<string, unknown>
 }
 
-export type APIResponse<T> = { success: true; data: T } | { success: false; error: APIErrorDetails }
+export type APIResponse<T> = { success: true; data: T; message: string } | { success: false; error: APIErrorDetails }
