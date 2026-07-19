762503dee369d361e3f42bc5e112ee713ff68e67
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-07-19 19:15:08 +0300

    refactor: Add support for fetching CSRF token to BFF `GET` endpoint on server route udner DOOMLITs route

--- src/routes/api/doomlits/[referenceId]/upload-urls/+server.ts
+++ src/routes/api/doomlits/[referenceId]/upload-urls/+server.ts
@@ -1,11 +1,15 @@
 import { json } from "@sveltejs/kit"
 
+import { getCsrfToken } from "repos/auth-repo"
 import { getUploadUrls } from "repos/project-repo"
 
 import type { RequestHandler } from "./$types"
 
 export const GET: RequestHandler = async ({ params, url, locals, fetch }) => {
+	const { csrfToken } = await getCsrfToken(fetch)
+	if (!csrfToken) return json({ message: "Security token missing. Please try again." }, { status: 403 })
+
 	const screenshotCount = Number(url.searchParams.get("screenshotCount") ?? "0")
-	const data = await getUploadUrls(params.referenceId, screenshotCount, locals.locale, fetch)
+	const data = await getUploadUrls(params.referenceId, screenshotCount, locals.locale, csrfToken, fetch)
 	return json(data)
 }
