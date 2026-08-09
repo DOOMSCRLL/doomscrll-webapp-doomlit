11fa9242f324abb1bfc029fbb968dd9fd04d5cea
Yakup Murat Çaylak <y.muratcaylak@outlook.com>
2026-08-09 17:58:22 +0300

    refactor: Add security and privacy related headers to the server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -23,9 +23,16 @@ export const handle: Handle = async ({ event, resolve }) => {
 	locale ??= LOCALE_DEFAULT
 	event.locals.locale = locale as Locale
 
-	return resolve(event, {
+	const response = await resolve(event, {
 		transformPageChunk: ({ html }) => html.replace("%lang%", locale),
 	})
+
+	response.headers.set("X-Frame-Options", "DENY")
+	response.headers.set("X-Content-Type-Options", "nosniff")
+	response.headers.set("Referrer-Policy", "strict-origin-when-cross-origin")
+	response.headers.set("Permissions-Policy", "camera=(), microphone=(), geolocation=()")
+
+	return response
 }
 
 export const handleFetch: HandleFetch = async ({ event, request, fetch }) => {
